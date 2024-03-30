0b8494af65b1e525428ee7d6aa0b1734f92b2660
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-03-30 14:00:28 +0530

    feat: Add pricing page
    - ignore node_modules from eslint

---
+++ lib/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+    {
+      name: 'Free',
+      slug: 'free',
+      quota: 10,
+      pagesPerPdf: 1,
+      price: {
+        amount: 0,
+        priceIds: {
+          test: '',
+          production: '',
+        },
+      },
+    },
+    {
+      name: 'Pro',
+      slug: 'pro',
+      quota: 50,
+      pagesPerPdf: 2,
+      price: {
+        amount: 5,
+        priceIds: {
+          test: '',
+          production: '',
+        },
+      },
+    },
+  ]
