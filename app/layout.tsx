c3e4ef2e4ce2481a77e2e7d566e4652632ac53a8
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-04 12:42:56 +0530

    fix: add toaster provider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
-import Navbar from "@/components/NavBar";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +23,8 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           {children}
+
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
