ca6d3bddff79a058a0525dc758278a3677b1a887
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-04 12:47:43 +0530

    feat: pdf renderer
    - fix: use raw loader to load `canva.node` (Otherwise it wont load, gives error)
    - add custom loader to webpack config rules.
    - change regex to match only `canvas.node` file else it will interfere with other `.node` files such as strip integrations

--- app/(routes)/dashboard/resume/[resumeId]/page.tsx
+++ app/(routes)/dashboard/resume/[resumeId]/page.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import PdfRenderer from '@/components/PdfRenderer';
+import { BUCKET_NAME } from '@/lib/constants';
+import { supabase } from '@/lib/supabse';
+import { auth } from '@clerk/nextjs';
+import { Loader2 } from 'lucide-react';
+import { Document } from 'react-pdf';
 
 interface ResumeProps {
-    params: {
-        resumeId: string;
-    }
+  params: {
+    resumeId: string;
+  }
 }
 
-const Resume = ({
-    params
+const Resume = async ({
+  params
 }: ResumeProps) => {
+  const { userId } = auth();
+  // TODO: get resume name action
+  // const resumeName = await getResumeName(userId, params.resumeId)
+
+  const { data } = await supabase.storage.from(BUCKET_NAME).getPublicUrl(`${userId}/Sudhanva_Nadiger.pdf`)
+
+  console.log(data);
+  
+
+  if (!data || !data.publicUrl) {
+    return <div>Resume not found</div>
+  }
+
   return (
-    <div>{params.resumeId}</div>
+    <PdfRenderer url={data.publicUrl} />
   )
 }
 
