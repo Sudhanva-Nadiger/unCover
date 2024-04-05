e95130ced55b7d8c54d1210c976aff1de6a6c325
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-05 13:23:08 +0530

    feat: fetch all resume

--- app/(routes)/dashboard/resume/page.tsx
+++ app/(routes)/dashboard/resume/page.tsx
@@ -1,8 +1,18 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import React from 'react'
 import UploadResumeButton from './components/UploadResumeButton'
+import { getAllResumes } from '@/lib/actions';
+import { auth } from '@clerk/nextjs';
 
-const ResumePage = () => {
+const ResumePage = async () => {
+  const { userId } = auth();
+  const [resumes, error] = await getAllResumes(userId);
+
+  if(error) {
+    return <div>{error.message || "Something went wrong"}</div>
+  }
+
+
   return (
     <MaxWidthWrapper className='mt-8 sm:mt-20'>
       <div className='flex items-center justify-center'>
