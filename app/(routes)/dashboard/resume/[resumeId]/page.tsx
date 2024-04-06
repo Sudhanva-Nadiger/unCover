ab9f21676dc5e3a6f89e1fe155c50a486452d800
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-06 14:42:21 +0530

    fix: use back arrow button in the page than in pdf renderer

--- app/(routes)/dashboard/resume/[resumeId]/page.tsx
+++ app/(routes)/dashboard/resume/[resumeId]/page.tsx
@@ -1,6 +1,11 @@
 import PdfRenderer from '@/components/PdfRenderer';
+import { buttonVariants } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 import { getResumeDetailsById } from '@/lib/actions/fileActions';
+import { cn } from '@/lib/utils';
 import { auth } from '@clerk/nextjs';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
 
 interface ResumeProps {
   params: {
@@ -12,15 +17,25 @@ const Resume = async ({
   params
 }: ResumeProps) => {
   const { userId } = auth();
-  
+
   const [resumeDetails, error] = await getResumeDetailsById(userId, params.resumeId)
 
-  if(error) {
+  if (error) {
     return <div>Resume not found</div>
   }
 
   return (
-    <PdfRenderer url={resumeDetails.url} showBackArrow={true} />
+    <div className='space-y-2 w-[90%]'>
+      <div className='flex flex-col justify-center items-center w-full space-x-3'>
+        <Link href={'/dashboard/resume'} className={cn(buttonVariants({
+          variant: 'outline'
+        }), 'mt-4')}>
+          <ArrowLeft className='w-6 h-r mr-2' />
+          Go back
+        </Link>
+      </div>
+      <PdfRenderer url={resumeDetails.url} />
+    </div>
   )
 }
 
