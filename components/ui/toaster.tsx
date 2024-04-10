e405be56fea1abfce8d3d94be082cb58465df7ae
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-10 13:27:48 +0530

    feat: add image component

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -9,20 +9,33 @@ import {
   ToastViewport,
 } from "@/components/ui/toast"
 import { useToast } from "@/components/ui/use-toast"
+import Image from "next/image"
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({ id, title, description, action, variant, ...props }) {
+        const showSuccessImage = (variant === 'default' || variant === undefined)
         return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
+          <Toast key={id} variant={variant} {...props}>
+            <div className="flex gap-4 items-center">
+              <div className="h-full aspect-square rounded-full">
+                <Image
+                  src={showSuccessImage ? '/success.png' : '/error.png'}
+                  alt="toast image"
+                  width={50}
+                  height={50}
+                  className="object-fill rounded-full"
+                />
+              </div>
+              <div className="grid gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
             </div>
             {action}
             <ToastClose />
