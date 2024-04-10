75a03c381874d41936df477a0cb7642cddfe4ef9
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-10 13:57:58 +0530

    feat: export meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
 import { UploadModalProvider } from "@/components/providers/UploadModalProvider";
+import { Toaster } from "@/components/ui/toaster";
+import "./globals.css";
+import { generateMetaData } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "unCover",
-  description: "Everything you need while applying for a job at one place.",
-  icons: ["/logo.png"]
-};
+export const metaddata = generateMetaData();
 
 export default function RootLayout({
   children,
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,54 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+
+
+export function generateMetaData({
+  title = "unCover - Everything you need while applying for a job at one place.",
+  description = "unCover is an open-source software to make creation of cover letter easy to apply for job.",
+  image = "/thumbnail.jpg",
+  icons = "/logo.png",
+  noIndex = false
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@sudhanvanadiger"
+    },
+    icons,
+    metadataBase: new URL('https://un-cover.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
 }
